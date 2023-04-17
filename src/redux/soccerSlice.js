69fb286b9958c94d2a4d2c2e06e3337bc8029070
sanieni6 <sanieni6@gmail.com>
2023-04-16 20:24:48 -0500

    fix linters

--- src/redux/soccerSlice.js
+++ src/redux/soccerSlice.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-unused-vars */
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const url = 'https://api-football-standings.azharimm.site/leagues';
 
 const initialState = {
-    rockets: [],
-    isLoading: true,
-    error: undefined,
-  };
+  rockets: [],
+  isLoading: true,
+  error: undefined,
+};
