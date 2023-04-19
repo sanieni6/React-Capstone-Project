50f3ae91cdc4acd554fae62144526cb2d5da23a6
sanieni6 <sanieni6@gmail.com>
2023-04-19 14:43:43 -0500

    Add some dependencies for testing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,17 @@ import {
 } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import '../styles/App.scss';
-import Leagues from './Leagues';
-import Standings from './Standings';
+import Leagues from '../pages/Leagues';
+import Standings from '../pages/Standings';
 
 function App() {
   const { leagueId } = useSelector((store) => store.standings);
+  const { leagues, isLoading, error } = useSelector((store) => store.leagues);
   return (
     <div className="App">
       <div>
         <Routes>
-          <Route path="/" element={<Leagues />} />
+          <Route path="/" element={<Leagues leagues={leagues} isLoading={isLoading} error={error} />} />
           <Route path="/standings/:id" element={<Standings leagueId={leagueId} />} />
         </Routes>
       </div>
